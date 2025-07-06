995e5e85b34a1478548e538e74381cae49108aaa
rdhmdhl <reid@wearthefewture.com>
2025-07-05 20:50:11 -0700

    separate backend

--- frontend/src/config/config.ts
+++
@@ -1,3 +0,0 @@
-const api_url =
-  "https://hlkxds5qoissqwwi3cihffchbm0anvko.lambda-url.us-west-2.on.aws";
-export default api_url;
--- frontend/src/types/AnswerMap.ts
+++
@@ -1,1 +0,0 @@
-export type AnswerMap = { [question: string]: string };
