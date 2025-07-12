8d3331b1ec5f4f86757b695cf524b806fadf3e6a
rdhmdhl <reid@wearthefewture.com>
2025-07-11 20:36:32 -0700

    update

--- main.go
+++ main.go
@@ -31,14 +31,13 @@ type Question struct {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS, HEAD, DELETE, PUT, PATCH")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Content-Type", "application/json")
 
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusOK)
-		return
 	}
 
 	var reqBody generateQuestionsReq
