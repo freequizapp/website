72935e352ffb089b08724a7a9f2e2db8dfa12e00
rdhmdhl <reid@wearthefewture.com>
2025-07-11 20:47:54 -0700

    update build fild

--- main.go
+++ main.go
@@ -30,7 +30,8 @@ type Question struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	origin := r.Header.Get("Origin")
+	w.Header().Set("Access-Control-Allow-Origin", origin)
 	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS, HEAD, DELETE, PUT, PATCH")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
